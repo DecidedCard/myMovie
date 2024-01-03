ecdd6a234b6a180b9516a399c9926d1361f9476a
jeong hea jun <heajun35@gmail.com>
2024-01-03 20:30:22 +0900

    정리

--- 개인과제1/script.js
+++ 개인과제1/script.js
@@ -1,24 +1,27 @@
 const options = {
-  method: 'GET',
+  method: "GET",
   headers: {
-    accept: 'application/json',
-    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhMzBjZjNhMjBlZGNhZjMxMmIwMjZhZjM1NzhiMTAyOCIsInN1YiI6IjY1OTRmNDQ5NTkwN2RlNDU5OTYzYmZkNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.jIu9EyJ2GTlw8ENNNk9IuC76BKQ_Ii0J4QAWX_-Jo00'
-  }
+    accept: "application/json",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhMzBjZjNhMjBlZGNhZjMxMmIwMjZhZjM1NzhiMTAyOCIsInN1YiI6IjY1OTRmNDQ5NTkwN2RlNDU5OTYzYmZkNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.jIu9EyJ2GTlw8ENNNk9IuC76BKQ_Ii0J4QAWX_-Jo00",
+  },
 };
 
-fetch('https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1', options)
-  .then(response => response.json())
-  .catch(err => console.error(err));
+fetch(
+  "https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1",
+  options
+).catch((err) => console.error(err));
 
+const url =
+  "https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1";
 
-const url = 'https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1';
-
-fetch(url).then(response => response.json().then (data => {
-let movie = data['results'];
-console.log(movie);
-for (let i = 0; i < movie.length; i++) {
-  const temp = document.createElement('div');
-  temp.innerHTML = `
+fetch(url).then((response) =>
+  response.json().then((data) => {
+    let movie = data["results"];
+    console.log(movie);
+    for (let i = 0; i < movie.length; i++) {
+      const temp = document.createElement("div");
+      temp.innerHTML = `
   <div class="container">
   <div class="top"><img src="https://image.tmdb.org/t/p/w200${movie[i].poster_path}"></div>
   <div class="bottom">
@@ -28,8 +31,7 @@ for (let i = 0; i < movie.length; i++) {
   </div>
 </div>`;
 
-document.getElementById('card').append(temp);
-}}
-  ))
-
-  
+      document.getElementById("card").append(temp);
+    }
+  })
+);
