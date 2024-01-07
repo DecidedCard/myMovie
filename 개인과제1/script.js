104fc92389af2c4795a53a9d5813454de17ebf71
jeong hea jun <heajun35@gmail.com>
2024-01-07 19:18:32 +0900

    검색기능 및 CSS추가

--- 개인과제1/script.js
+++ 개인과제1/script.js
@@ -6,29 +6,23 @@ const options = {
       "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhMzBjZjNhMjBlZGNhZjMxMmIwMjZhZjM1NzhiMTAyOCIsInN1YiI6IjY1OTRmNDQ5NTkwN2RlNDU5OTYzYmZkNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.jIu9EyJ2GTlw8ENNNk9IuC76BKQ_Ii0J4QAWX_-Jo00",
   },
 };
-
-fetch(
-  "https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1",
-  options
-).catch((err) => console.error(err));
-
 const url =
   "https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1";
 
-fetch(url).then((response) =>
+fetch(url, options).then((response) =>
   response.json().then((data) => {
     let movie = data["results"];
     for (let i = 0; i < movie.length; i++) {
       const temp = document.createElement("div");
       temp.innerHTML = `
-  <div id="container" class="container" onclick = "checkId(${movie[i].id})">
+  <div id="container" class="container" >
 <div class="top">
-<img id = "image" src="https://image.tmdb.org/t/p/w200${movie[i].poster_path}">
+<img id = "image" src="https://image.tmdb.org/t/p/w200${movie[i].poster_path}" onclick = "checkId(${movie[i].id})">
   </div>
   <div class="bottom">
-    <div class="title">${movie[i].title}</div>
-    <div class="overview">${movie[i].overview}</div>
-    <div class="vote_average">${movie[i].vote_average}</div>
+    <div class="title">영화제목: ${movie[i].title}</div>
+    <div class="overview">내용: ${movie[i].overview}</div>
+    <div class="vote_average">별점: ${movie[i].vote_average}</div>
   </div>
 </div>`;
 
@@ -42,16 +36,17 @@ function checkId(num) {
 }
 
 function serchMovie() {
-  let str = document.getElementById('info').value;
-  console.log(str);
-  let checkCard = document.getElementsByClassName('container');
-  let checkTitle = document.getElementsByClassName('title');
+  let str = document.getElementById("info").value;
+  let checkCard = document.getElementsByClassName("container");
+  let checkTitle = document.getElementsByClassName("title");
   for (let i = 0; i < checkCard.length; i++) {
-    let a = checkTitle[i].innerHTML
-      for (let p = 0; p < str.length; p++) {
-        if (str[p].toUppesCase().includes(a.toUpperCase())){
-          console.log('성공');
-        }
-      }
+    let a = checkTitle[i].innerHTML;
+    if (!a.includes(str)) {
+      container[i].style.display = "none";
+    } else {
+      container[i].style.display = "block";
+    }
   }
 }
+
+fetch(url, options).catch((err) => console.error(err));
