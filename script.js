57e976e3512b212324e1022654342219b2136875
jeong hea jun <heajun35@gmail.com>
2024-01-08 17:27:33 +0900

    수정 및 추가

--- script.js
+++ script.js
@@ -3,11 +3,10 @@ const options = {
   headers: {
     accept: "application/json",
     Authorization:
-      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhMzBjZjNhMjBlZGNhZjMxMmIwMjZhZjM1NzhiMTAyOCIsInN1YiI6IjY1OTRmNDQ5NTkwN2RlNDU5OTYzYmZkNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.jIu9EyJ2GTlw8ENNNk9IuC76BKQ_Ii0J4QAWX_-Jo00",
-  },
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhMzBjZjNhMjBlZGNhZjMxMmIwMjZhZjM1NzhiMTAyOCIsInN1YiI6IjY1OTRmNDQ5NTkwN2RlNDU5OTYzYmZkNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.jIu9EyJ2GTlw8ENNNk9IuC76BKQ_Ii0J4QAWX_-Jo00"
+  }
 };
-const url =
-  "https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1";
+const url = "https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1";
 
 fetch(url, options).then((response) =>
   response.json().then((data) => {
@@ -16,40 +15,36 @@ fetch(url, options).then((response) =>
     for (let i = 0; i < movie.length; i++) {
       const temp = document.createElement("div");
       temp.innerHTML = `
-  <div id="container" class="container" >
-<div class="top">
-<img id = "image" class = "image" src="https://image.tmdb.org/t/p/w200${movie[i].poster_path}" onclick = "checkId(${movie[i].id})">
-  </div>
-  <div class="bottom">
-    <div class="title"><span>영화제목</span>: <br>${movie[i].title}</div><br>
-    <div class="overview"><span>내용</span>: ${movie[i].overview}</div><br>
-    <div class="vote_average"><span>별점</span>: ${movie[i].vote_average}</div>
-  </div>
-</div>`;
+  <li id="container" class="container" >
+  <img 
+  id = "image" 
+  class = "image" 
+  src="https://image.tmdb.org/t/p/w200${movie[i].poster_path}" 
+  onclick = "alert('영화 아이디는 ${movie[i].id}입니다.')">
+  <h2 class="movieTitle">영화 제목</h2>
+    <p class="title"> ${movie[i].title}</p><br>
+    <p class="overview"><span>내용</span>: ${movie[i].overview}</p><br>
+    <p class="vote_average"><span>별점</span>: ${movie[i].vote_average}</p>
+  </li>`;
 
       document.getElementById("card").append(temp);
     }
   })
 );
 
-// 이미지를 클릭했을때 영와 id가 alert창으로 뜨게 구현, 영화 id값을 함수 매개변수로 넣었음.
-function checkId(num) {
-  alert("영화 id : " + num);
-}
+fetch(url, options).catch((err) => console.error(err));
 
-// 검색기능
+//검색기능
 function serchMovie() {
-  let str = document.getElementById("info").value;
-  let checkCard = document.getElementsByClassName("container");
-  let checkTitle = document.getElementsByClassName("title");
-  for (let i = 0; i < checkCard.length; i++) {
-    let a = checkTitle[i].innerHTML;
-    if (!a.includes(str)) {
+  const str = document.querySelector(".info").value;
+  const checkTitle = document.querySelectorAll(".title");
+  for (let i = 0; i < checkTitle.length; i++) {
+    const a = checkTitle[i].innerHTML;
+    if (!a.toUpperCase().includes(str.toUpperCase())) {
       container[i].style.display = "none";
     } else {
       container[i].style.display = "block";
     }
   }
+  event.preventDefault();
 }
-
-fetch(url, options).catch((err) => console.error(err));
